cf481a45484f99c8fba7bb5f2b8fd16adfc9aaac
kingjames500 <jimmymaina500@gmail.com>
2024-09-13 10:47:52 +0300

    sum of two numbers

--- sumOfTwoNumbers.js
+++ sumOfTwoNumbers.js
@@ -3,4 +3,4 @@ console.log("==== sumOfTwoNumbers ====");
 function sumOfTwoNumbers(num1, num2){
     return num1 + num2;
 }
-console.log(sumOfTwoNumbers(10, 20));
+console.log(sumOfTwoNumbers(13, 20));
