b694f2eb0af746c3347928f1714962e39d3faf04
kingjames500 <jimmymaina500@gmail.com>
2024-09-13 11:09:06 +0300

    A function to return the next number from the integer passsed

---
+++ incrementFromNumberPassed.js
@@ -0,0 +1,9 @@
+console.log("=== returning the next number form the integer passed ===");
+
+function IncrementFromNumberPassed(number){
+    return number +1;
+}
+
+let nextNumber = IncrementFromNumberPassed(-3);
+
+console.log(nextNumber);
