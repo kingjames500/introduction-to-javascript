633046b195ab03d75dd2264e8c75fa69d56d8ce4
kingjames500 <jimmymaina500@gmail.com>
2024-09-13 10:56:21 +0300

    area of a right angled triangle

---
+++ areaOfATriangle.js
@@ -0,0 +1,10 @@
+//This Function will calculate the area of a right angled traingle
+console.log("=== areaOfATriangle ===");
+
+function areaOfATriangle(base, height){
+    return (base * height) /2;
+}
+
+let area = areaOfATriangle(5, 10);
+
+console.log(area); 
